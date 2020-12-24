a2210a3119f0691a798a57e5c3520e594bd77fa6
luckyraj810 <72134374+luckyraj810@users.noreply.github.com>
2020-12-24 09:53:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+var hr,mn,sc;
+var hrAngle,mnAngle,scAngle;
+
+
+function setup() {
+  createCanvas(400,400);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(0); 
+  stroke("red");
+  strokeWeight(2);
+  text("Hey Time is: "+hr +": "+mn+": "+sc,280,350);
+  translate(200,200);
+  rotate(-90);
+
+  hr=hour();
+  mn=minute();
+  sc=second();
+
+  scAngle=map(sc,0,60,0,360);
+  mnAngle=map(mn,0,60,0,360);
+  hrAngle=map(hr%12,0,12,0,360);
+
+  push();
+  rotate(scAngle);
+  stroke(0,225,0);
+  strokeWeight(7);
+  line(0,0,100,0);
+  pop();
+
+  push();
+  rotate(mnAngle);
+  stroke(225,0,0);
+  strokeWeight(7);
+  line(0,0,75,0);
+  pop();
+
+  push();
+  rotate(hrAngle);
+  stroke(0,0,225);
+  strokeWeight(7);
+  line(0,0,50,0);
+  pop();
+
+  stroke(255,0,255);
+  point(0,0);
+
+  strokeWeight(10);
+  noFill();
+
+  stroke(0,225,0);
+  arc(0,0,300,300,0,scAngle);
+
+  stroke(255,0,0);
+  arc(0,0,280,280,0,mnAngle);
+
+  stroke(0,0,225);
+  arc(0,0,260,260,0,hrAngle);
+
+ 
+}
